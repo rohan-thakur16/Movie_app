b9e248edee60ff35cab12f2ba1f6c2956b6946b8
rohan-thakur16 <94912422+rohan-thakur16@users.noreply.github.com>
2023-05-16 19:34:42 +0100

    Adding splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,40 @@
 
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_api/screens/home_screen.dart';
 
 
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Assignment',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blueGrey,
+//       ),
+//       home: const HomePage(),
+//     );
+//   }
+// }
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Assignment',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: const HomePage(),
-    );
+        title: 'Flutter Assignment',
+        home: AnimatedSplashScreen(
+            duration: 2000,
+            splash: 'assets/images/sm_icon.png',
+            nextScreen: HomePage(),
+            splashTransition: SplashTransition.rotationTransition,
+            // pageTransitionType: PageTransitionType.scale,
+            backgroundColor: Colors.blueGrey));
   }
 }
